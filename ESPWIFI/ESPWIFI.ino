7009267897da5b325ab4ed564e5a995dc8a2eed8
Vivek0712 <vivekraja98@gmail.com>
2017-10-06 14:45:15 +0530

    Wifi Setup function

--- ESPWIFI/ESPWIFI.ino
+++ ESPWIFI/ESPWIFI.ino
@@ -8,3 +8,25 @@ int interval=10000;
 String answer;
 
 WiFiServer server(80);
+
+
+void setup(void)
+{  
+  Serial.begin(115200);
+  
+  // Connect to WiFi network
+  WiFi.begin(ssid, password);
+
+  Serial.println("STARTING WIFI");
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println(".");
+  }
+  Serial.println("CONNECTED");
+  delay(500);    
+  // Start TCP (HTTP) server
+  server.begin();  
+  Serial.println(WiFi.localIP());
+}
+
